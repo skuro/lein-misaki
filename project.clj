32522942051ef660354a134d669d3424b8c026b8
Carlo Sciolla <skuro@skuro.tk>
2012-10-14 09:15:51 +0200

    Bump version number for the next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-misaki "0.1.0"
+(defproject lein-misaki "0.1.1-SNAPSHOT"
   :description "A Leiningen plugin for the Misaki static sites generator"
   :url "https://github.com/skuro/lein-misaki"
   :license {:name "Eclipse Public License"
