68eba590627f1bbed2539d359aa80f800eec4be6
liquidz <liquidz.uo@gmail.com>
2012-10-15 00:10:31 +0900

    Add 'new' command to generate a misaki project

--- src/leiningen/misaki.clj
+++ src/leiningen/misaki.clj
@@ -1,12 +1,31 @@
 (ns leiningen.misaki
-  (:use [misaki server config]))
+  (:use [misaki server config])
+  (:require [gh-file-reader.core  :as gh]
+            [text-decoration.core :as td]))
 
 (defn get-current-directory []
   (-> "."
       java.io.File.
       .getCanonicalPath))
 
+(defn misaki-new
+  "Generate a new Misaki project"
+  ([project-name]
+   (misaki-new project-name "default"))
+  ([project-name template-name]
+   (println
+     (str "Generating a misaki project called " project-name
+          " based on the '" template-name "' template."))
+   ; download template
+   (if-not (gh/download
+         (gh/read-content "liquidz" "misaki-showcase"
+                          (str "/templates/" template-name "/files"))
+         project-name)
+     (println (td/red (str "Template '" template-name "' is not found"))))))
+
 (defn ^:no-project-needed misaki
   "Compiles your Misaki sources and starts a local server"
   [project & args]
-  (apply -main (get-current-directory) args))
+  (case (first args)
+    "new" (apply misaki-new (rest args))
+    (apply -main (get-current-directory) args)))
