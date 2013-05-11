83fb772d4aec0349401695f412087fb7dbbb7365
Carlo Sciolla <skuro@skuro.tk>
2013-05-11 17:59:41 +0200

    #2 Added support for misaki-dependencies in your project.clj or profiles.clj (of course, when using a project.clj there's no real need for this trick)

--- src/leiningen/misaki.clj
+++ src/leiningen/misaki.clj
@@ -1,5 +1,6 @@
 (ns leiningen.misaki
-  (:use [misaki server config])
+  (:use [misaki server config]
+        [cemerick.pomegranate :only [add-dependencies]])
   (:require [gh-file-reader.core  :as gh]
             [text-decoration.core :as td]
             [leiningen.misaki.github :as github]))
@@ -24,9 +25,21 @@
          project-name)
      (println (td/red (str "Template '" template-name "' is not found"))))))
 
+(defn load-misaki-deps
+  "Loads the misaki dependencies as specified by the :misaki-dependency entry
+   in your project.clj or profiles.clj"
+  [deps]
+  (println (str "Loading the following misaki plugins:"))
+  (doseq [dep deps]
+    (apply println dep))
+  (add-dependencies :coordinates deps
+                    :repositories {"clojars" "http://clojars.org/repo"}))
+
 (defn ^:no-project-needed misaki
   "Compiles your Misaki sources and starts a local server"
   [project & args]
+  (if-let [deps (:misaki-dependencies project)]
+    (load-misaki-deps deps))
   (case (first args)
     "new" (apply misaki-new (rest args))
     "listen" (apply github/listen (rest args))
