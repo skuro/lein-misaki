e82632314c35072742b3d94deb84c3c345eeaab3
Carlo Sciolla <skuro@skuro.tk>
2013-05-11 18:14:30 +0200

    Back to SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-misaki "0.1.2"
+(defproject lein-misaki "0.1.3-SNAPSHOT"
   :description "A Leiningen plugin for the Misaki static sites generator"
   :url "https://github.com/skuro/lein-misaki"
   :license {:name "Eclipse Public License"
