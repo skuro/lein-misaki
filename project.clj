9f90eae3723f26be9ad4afc4e3a58dc433899368
Carlo Sciolla <skuro@skuro.tk>
2013-06-25 23:49:47 +0200

    lein-release plugin: preparing 0.1.3 release

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject lein-misaki "0.1.3-SNAPSHOT"
+(defproject lein-misaki "0.1.3"
   :description "A Leiningen plugin for the Misaki static sites generator"
   :url "https://github.com/skuro/lein-misaki"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[misaki "0.2.3-beta"]
+  :dependencies [[misaki "0.2.6.2-beta"]
                  [gh-file-reader "0.0.3"]
                  [clj-text-decoration "0.0.1"]
                  [compojure "1.1.3"]
