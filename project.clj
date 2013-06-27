3dd3e8097604cbf774ff044db2886227d91beeeb
Carlo Sciolla <skuro@skuro.tk>
2013-06-27 12:44:19 +0200

    Finalize v0.1.4 to include support for gh-pages

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-misaki "0.1.4-SNAPSHOT"
+(defproject lein-misaki "0.1.4"
   :description "A Leiningen plugin for the Misaki static sites generator"
   :url "https://github.com/skuro/lein-misaki"
   :license {:name "Eclipse Public License"
