42b95e10b35cf86f886ebb206994ed924db0154c
Carlo Sciolla <skuro@skuro.tk>
2014-07-07 10:48:12 +0200

    Removed conflicting dependencies already brought in by misaki

--- project.clj
+++ project.clj
@@ -5,9 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[misaki "0.2.6.2-beta"]
                  [gh-file-reader "0.0.3"]
-                 [clj-text-decoration "0.0.1"]
-                 [compojure "1.1.3"]
                  [clj-jgit "0.6.3"]
-                 [me.raynes/fs "1.4.4"]
-                 [ring/ring-jetty-adapter "1.1.0"]]
+                 [me.raynes/fs "1.4.4"]]
   :eval-in-leiningen true)
